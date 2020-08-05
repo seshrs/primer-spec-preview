5e75842ee6ef95f81473c98d091e0d47f967012d
Sesh Sadasivam <seshrs@umich.edu>
2020-08-05 11:33:55 -0400

    Another attempt at removing stale configs

--- server.rb
+++ server.rb
@@ -263,7 +263,8 @@ class GHAapp < Sinatra::Application
       if delete_gemfile && File.exists?("#{@full_repo_name}/Gemfile")
         FileUtils.rm("#{@full_repo_name}/Gemfile")
       end
-      if delete_config_file && File.exists?("#{@site_location}/_config.yml")
+      Dir.chdir @site_location
+      if delete_config_file && File.exists?("_config.yml")
         FileUtils.rm("#{@site_location}/_config.yml")
       end
       return success
